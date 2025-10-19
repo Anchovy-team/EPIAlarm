1be5ae920a38a76026d8a7af85559e576069664e
Winnerty <timofey.n.karpeev@gmail.com>
2025-10-19 02:15:07 +0200

    codestyle fix

--- app/src/main/java/anchovy/team/epialarm/NotificationsBroadcastReceiver.java
+++ app/src/main/java/anchovy/team/epialarm/NotificationsBroadcastReceiver.java
@@ -6,7 +6,6 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.media.RingtoneManager;
 import androidx.annotation.RequiresPermission;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
